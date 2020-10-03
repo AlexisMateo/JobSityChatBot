ae3f47fe00bd03a7013ba824bfa09d4bd7ad8250
Rafael Contreras <r.contreras@C02R3CMLG8WP.local>
2020-10-03 14:39:05 -0400

    [add] basic login process

--- JobSity.ChatApp.IdentityServer/Controllers/AuthenticationController.cs
+++ JobSity.ChatApp.IdentityServer/Controllers/AuthenticationController.cs
@@ -1,16 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
 
 namespace JobSity.ChatApp.IdentityServer.Controllers
 {
     public class AuthenticationController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
-        public AuthenticationController(UserManager<IdentityUser> userManager)
+        public AuthenticationController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IActionResult Index()
@@ -18,13 +21,38 @@ namespace JobSity.ChatApp.IdentityServer.Controllers
             return View();
         }
 
-        public IActionResult Login()
+        public async Task<IActionResult> Login(string userName, string password)
         {
+            var user = await _userManager.FindByNameAsync(userName);
+
+            if(user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, password, false, false);
+
+                if(result.Succeeded)
+                {
+                    return Redirect("Index");
+                }
+            }
+
             return View();
         }
 
-        public IActionResult Register()
+        public async Task<IActionResult> Register(string userName, string password)
         {
+            var user = new IdentityUser
+            {
+                UserName = userName,
+                Email = string.Empty
+            };
+
+            var result = await _userManager.CreateAsync(user, password);
+
+            if(result.Succeeded)
+            {
+
+            }
+
             return View();
         }
 
