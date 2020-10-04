eed503879c85c9908ce4ea7e499f98839df08f04
Rafael Contreras <r.contreras@C02R3CMLG8WP.local>
2020-10-04 18:20:05 -0400

    [add] generated migration for chatromm message

---
+++ JobSity.ChatApp.Infrastructure/Migrations/FinancialChatDb/20201004221317_InitialFinancialChatRoom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JobSity.ChatApp.Infrastructure.Migrations.FinancialChatDb
+{
+    public partial class InitialFinancialChatRoom : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    MessageId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(nullable: true),
+                    MessageText = table.Column<string>(nullable: true),
+                    SentDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.MessageId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
