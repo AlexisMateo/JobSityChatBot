cf8d525b7a3aebfb71ae05a7cb90d6e4fb47e0a5
Rafael Contreras <r.contreras@C02R3CMLG8WP.local>
2020-10-06 15:19:06 -0400

    [add] Executing migration at start up Just for dev

--- JobSity.ChatApp.Api/Program.cs
+++ JobSity.ChatApp.Api/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JobSity.ChatApp.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +16,16 @@ namespace JobSity.ChatApp.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                // ToDo Just for development purporse. Should be removed for production.
+                var db = scope.ServiceProvider.GetRequiredService<FinancialChatDbContext>();
+                db.Database.Migrate();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- JobSity.ChatApp.IdentityServer/Program.cs
+++ JobSity.ChatApp.IdentityServer/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JobSity.ChatApp.Infrastructure.Repositories;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,13 +16,24 @@ namespace JobSity.ChatApp.IdentityServer
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                // ToDo Just for development purporse. Should be removed for production.
+                var db = scope.ServiceProvider.GetRequiredService<IdentityChatDbContext>();
+                db.Database.Migrate();
+            }
+
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    
                     webBuilder.UseStartup<Startup>();
                 });
     }
